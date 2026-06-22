22c3a0e445133ccba85ffa646d6ec33338d69dd7
Vastvik Sharma <vastviksharma8051@gmail.com>
2026-06-22 14:47:31 +0530

    home done

--- script.js
+++ script.js
@@ -61,3 +61,51 @@ setInterval(() => {
     showSlide(currentSlide);
 
 }, 6000);
+
+
+// notif tab 
+const products = document.querySelector(".categories");
+
+const popup = document.querySelector(".hamper-popup");
+const mini = document.querySelector(".hamper-mini");
+const closeBtn = document.querySelector(".close-popup");
+
+let shown = false;
+
+window.addEventListener("scroll", () => {
+
+    if(shown) return;
+
+    const sectionTop = products.offsetTop;
+    const scrollPos = window.scrollY + window.innerHeight;
+
+    if(scrollPos > sectionTop + 150){
+
+        shown = true;
+
+        popup.classList.add("show");
+
+        setTimeout(() => {
+
+            popup.classList.remove("show");
+            mini.classList.add("show");
+
+        }, 5000);
+
+    }
+
+});
+
+closeBtn.addEventListener("click", () => {
+
+    popup.classList.remove("show");
+    mini.classList.add("show");
+
+});
+
+mini.addEventListener("click", () => {
+
+    popup.classList.add("show");
+    mini.classList.remove("show");
+
+});
